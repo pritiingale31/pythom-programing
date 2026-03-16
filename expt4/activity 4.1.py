8fd466154dd587af9afa337db62883838b3926f0
pritiingale31 <pritiingale696@gmail.com>
2026-03-16 10:41:11 +0530

    Add files via upload

---
+++ expt4/activity 4.1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 04:52:59 2026
+
+@author: User
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:32:32 2026
+
+@author: priti
+"""
+
+
+marks = [78, 85, 90, 67, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
