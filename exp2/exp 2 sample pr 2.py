f6a64a1fb698cb76980da03c27a63e05c214e200
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:28:29 +0530

    Add files via upload

---
+++ exp2/exp 2 sample pr 2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 08:59:14 2026
+
+@author: priti ingle
+"""
+n = int(input("Enter number:"))
+fact = 1
+for i in range(1, n + 1):
+fact = fact * i
+print("Factorial:", fact)
