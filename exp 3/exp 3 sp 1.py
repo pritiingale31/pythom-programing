cd254ccbbe86b5d3736dcfa6da460575268ec1eb
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:54:04 +0530

    Add files via upload

---
+++ exp 3/exp 3 sp 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:09:26 2026
+
+@author: priti ingle
+"""
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+for j in range(1, i + 1):
+print(j, end=" ")
+print()
