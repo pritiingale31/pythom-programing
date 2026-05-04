61da1dc13c6bfbbf8fd700d34c3d87cf003f2f9d
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:52:39 +0530

    Add files via upload

---
+++ exp 3/exp 3 activity 3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:34:08 2026
+
+@author: priti ingle
+"""
+for num in range(1, 11):
+    print(f"Multiplication Table for {num}:")
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+    print("-" * 20)
+
