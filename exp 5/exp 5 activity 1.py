a75cf784e5a80650234e7b459cafeb9730cfdce0
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:39:20 +0530

    Add files via upload

---
+++ exp 5/exp 5 activity 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:45:58 2026
+
+@author: priti ingale
+"""
+# Initial library data
+library = {
+    "Python Basics": 500,
+    "Data Science 101": 750,
+    "Machine Learning": 1200
+}
+
+# Update price for a specific book
+library["Python Basics"] = 550
+
+print(f"Updated Library: {library}")
+
