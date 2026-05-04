940d0581ec9f1242e53096c649f9bfb4d4544fb3
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:40:48 +0530

    Add files via upload

---
+++ exp 5/exp 5 sp 1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:42:22 2026
+
+@author: priti ingale
+"""
+# Creating an empty dictionary
+student = {}
+# Adding key-value pairs
+student["name"] = "Rahul"
+student["age"] = 20
+student["course"] = "Python"
+print("Dictionary after adding elements:")
+print(student)
+# Updating a value
+student["age"] = 21
+print("\nDictionary after updating a value:")
+print(student)
+# Deleting a key-value pair
+del student["course"]
+print("\nDictionary after deleting a key-value pair:")
+print(student)
