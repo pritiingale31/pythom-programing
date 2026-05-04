84acc08ea0a92f024fec80d87dfcbc52cdb50ab0
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:33:58 +0530

    Add files via upload

---
+++ exp 4/exp 4 activity 2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:40:31 2026
+
+@author: priti ingale
+"""
+prices = [499.0, 1200.5, 150.0, 25.0]
+
+total_bill = sum(prices)
+
+print(f"Total Bill: ₹{total_bill}")
+
