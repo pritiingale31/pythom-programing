4bbace31bd8f0f0b9325e6958203332f7059b79b
pritiingale31 <pritiingale696@gmail.com>
2026-05-04 15:33:33 +0530

    Add files via upload

---
+++ exp 4/exp 4 activity 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:38:59 2026
+
+@author: priti ingle
+"""
+marks = [85, 92, 78, 95, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print(f"Average Marks: {average}")
+print(f"Topper Marks: {topper}")
+
